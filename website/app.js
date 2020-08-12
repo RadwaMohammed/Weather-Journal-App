32a749cf80ee920c4aaf6b1e36b2dacf640ebbb8
radwa <mail2radwa@gmail.com>
2020-08-12 09:32:18 +0200

    fix: Fix the baseURL

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 /* Global Variables */
 /* API credentials for OpenWeatherMap */
 const apikey = '96e40292186f4edc8eda790705381722';
-const baseURL = 'http://penweathermap.org/data/2.5/weather?zip=';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();

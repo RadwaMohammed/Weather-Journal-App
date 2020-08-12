54b0601a7c004497d5d88db44c10bd46c503b04c
radwa <mail2radwa@gmail.com>
2020-08-12 07:06:31 +0200

    chore: Add API credentials

--- website/app.js
+++ website/app.js
@@ -1,4 +1,7 @@
 /* Global Variables */
+/* API credentials for OpenWeatherMap */
+const apikey = '96e40292186f4edc8eda790705381722';
+const baseURL = 'http://penweathermap.org/data/2.5/weather?zip=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();

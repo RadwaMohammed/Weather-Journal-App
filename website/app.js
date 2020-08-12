d2a8ba326b51f1d063e1f7042b7224c00ddbca65
radwa <mail2radwa@gmail.com>
2020-08-12 07:59:42 +0200

    feat: Create async GET request

--- website/app.js
+++ website/app.js
@@ -6,3 +6,17 @@ const baseURL = 'http://penweathermap.org/data/2.5/weather?zip=';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Create async function to make GET request to OpenWeatherMap API
+const getData = async (baseURL, zipCode, apikey) => {
+    // Get weather info from OpenWeatherMap.org
+    const res = await fetch(`${baseURL}${zipCode}&appid=${apikey}`);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch(error) {
+        // Handle error
+        console.log("error", error);
+    }
+};
